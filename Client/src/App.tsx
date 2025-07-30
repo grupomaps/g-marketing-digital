25e31a9f3ea457b1e35d90a303f6bc2c6358c2dc
Guilherme Silva <guissilval005@gmail.com>
2025-07-30 13:44:22 -0300

    addicionando anallytics

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,7 +1,12 @@
 import { LocalRoutes } from "./app/routes/LocalRoutes";
+import { Analytics } from "@vercel/analytics/react"
+
 
 export const App = () => {
   return (
+    <>
+      <Analytics />
     <LocalRoutes />
+    </>
   );
 }
